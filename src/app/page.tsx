4092b953506f75f224370b7de07629560b6641bd
Andre Costa <andre2396costa@gmail.com>
2026-05-02 16:17:49 -0300

    usando react-hook-form com libs de ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import { SingUpForm } from "@/types/SingUpForm";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Input } from "./components/Input";
+import { Input } from "@mui/material";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 
 export default function Home() {
 
-  const { handleSubmit, control, setValue } = useForm<SingUpForm>();
+  const { handleSubmit, control } = useForm<SingUpForm>();
 
   const handleFormSubmit: SubmitHandler<SingUpForm> = (data) => {
     console.log(data);
@@ -17,28 +17,29 @@ export default function Home() {
     <div className="container mx-auto">
         <form onSubmit={handleSubmit(handleFormSubmit)}>
 
-          <Input 
+          <Controller 
             control={control}
             name="name"
-            rules={{required: true, minLength: 2}}
+            render={({ field, fieldState }) => <Input { ...field } value={field.value ?? ""} error={fieldState.invalid} />}
+            rules={{required: true, minLength: 2, maxLength: 20}}
           />
 
-          <Input 
+          <Controller 
             control={control}
             name="password"
-            rules={{required: true, minLength: 2}}
+            render={({ field, fieldState }) => <Input { ...field } value={field.value ?? ""} error={fieldState.invalid} />}
           />
 
-          <Input 
+          <Controller 
             control={control}
             name="age"
-            rules={{required: 'Informe seu nome', min: 18, max: 120 }}
+            render={({ field, fieldState }) => <Input { ...field } value={field.value ?? ""} error={fieldState.invalid} />}
+            rules={{required: true, min: 18, max: 120}}
           />
 
           <input type="submit" value="Enviar" />
         </form>
 
-        <button onClick={() => setValue('age', 18)}>Definir idade</button>
     </div>
   );
 }
